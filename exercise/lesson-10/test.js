2270cb8cfe0a649da1588edb2557ffc9e297c676
Rohan Koli <kolirohan143@gmail.com>
2026-03-14 18:42:04 +0530

    added exercise solution with demo code

--- exercise/lesson-10/test.js
+++ exercise/lesson-10/test.js
@@ -17,8 +17,20 @@ function removePreviousToggle(){
 
 function calculate(){
   let inputElement=Number(document.querySelector('.input').value);
+  document.querySelector('.shipping-result').innerHTML='';
+  document.querySelector('.error-message').innerHTML='';
+  if(inputElement<0){
+    document.querySelector('.error-message').innerHTML='Error: cost cannot be less than $0'
+    return;
+  }
   if(inputElement<40){
     inputElement=inputElement+10;
   }
   document.querySelector('.shipping-result').innerHTML=`$${inputElement}`;
+}
+
+function keypress(event){
+  if(event.key=='Enter'){
+    calculate();
+  }
 }
